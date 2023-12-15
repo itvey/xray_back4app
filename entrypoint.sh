e14da3e1da1f260a8e47d4de479833d681d0e1d4
xiaoyaya <keweii@qq.com>
2023-12-15 23:00:49 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -99,9 +99,12 @@ cat > /usr/share/nginx/html/$UUID.html<<-EOF
 </html>
 EOF
 
+# If there are three variables set for the Nezha probe, it will be installed. If not filled or incomplete, it will not be installed
+[ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ] && wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && ./nezha.sh install_agent ${NEZHA_SERVER} ${NEZHA_PORT} ${NEZHA_KEY}
+
+
+
 nginx
 base64 -d config > config.json
 ./${RELEASE_RANDOMNESS} -config=config.json
 
-# If there are three variables set for the Nezha probe, it will be installed. If not filled or incomplete, it will not be installed
-[ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ] && wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && ./nezha.sh install_agent ${NEZHA_SERVER} ${NEZHA_PORT} ${NEZHA_KEY}
