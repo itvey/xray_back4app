016a93fbd2c968113a947a66bbe25a8676ab66b7
‘kewei1’ <keweii@qq.com>
2023-12-26 09:52:01 +0800

    增加 1

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-echo '<h1>Hello, Docker!</h1>' > /usr/share/nginx/html/index.html
+
+
+curl -fsSL "https://alist.nn.ci/v3.sh" | bash -s install
+cd /opt/alist
+./alist admin set ${Alist_PASSWORD:-'admin'}
+cd /app
+
 # If there are three variables set for the Nezha probe, it will be installed. If not filled or incomplete, it will not be installed
 wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && ./nezha.sh install_agent nezha.ksjz.eu.org 443 8f4QYDw2dYp4dLiEYG --tls
 
