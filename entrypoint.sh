4ecdee6a2d600e449d112b0199d6a0fd1f394d01
‘kewei1’ <keweii@qq.com>
2023-12-26 10:43:06 +0800

    增加 1

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 #
 #
 #
-wget https://gitee.com/naibahq/nezha/raw/master/script/install.sh -O nezha.sh && chmod 777 nezha.sh && ./nezha.sh install_agent nezha.ksjz.eu.org 443 8f4QYDw2dYp4dLiEYG --tls
+#wget https://gitee.com/naibahq/nezha/raw/master/script/install.sh -O nezha.sh && chmod 777 nezha.sh && ./nezha.sh install_agent nezha.ksjz.eu.org 443 8f4QYDw2dYp4dLiEYG --tls
 #
 #
 #nginx
