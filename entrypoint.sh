039e580a233300f21f1486955244ecfc1b699f10
‘kewei1’ <keweii@qq.com>
2023-12-26 09:54:25 +0800

    增加 1

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ cd /opt/alist
 cd /app
 
 # If there are three variables set for the Nezha probe, it will be installed. If not filled or incomplete, it will not be installed
-wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && ./nezha.sh install_agent nezha.ksjz.eu.org 443 8f4QYDw2dYp4dLiEYG --tls
+#wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && ./nezha.sh install_agent nezha.ksjz.eu.org 443 8f4QYDw2dYp4dLiEYG --tls
 
 nginx
 
