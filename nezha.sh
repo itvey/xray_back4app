5bd7b8292da9635a8ccec6ef074e69ff5f46b124
‘kewei1’ <keweii@qq.com>
2023-12-27 15:43:18 +0800

    增加 1

--- nezha.sh
+++ nezha.sh
@@ -68,21 +68,21 @@ pre_check() {
         fi
     fi
 
-    if [[ -z "${CN}" ]]; then
-        GITHUB_RAW_URL="gitee.com/naibahq/nezha/raw/master"
-        GITHUB_URL="dn-dao-github-mirror.daocloud.io"
-        Get_Docker_URL="get.docker.com"
-        Get_Docker_Argu=" -s docker --mirror Aliyun"
-        Docker_IMG="registry.cn-shanghai.aliyuncs.com\/naibahq\/nezha-dashboard"
-        GITHUB_RELEASE_URL="hub.fgit.cf/naiba/nezha/releases/latest/download"
-    else
-        GITHUB_RAW_URL="gitee.com/naibahq/nezha/raw/master"
-        GITHUB_URL="dn-dao-github-mirror.daocloud.io"
-        Get_Docker_URL="get.docker.com"
-        Get_Docker_Argu=" -s docker --mirror Aliyun"
-        Docker_IMG="registry.cn-shanghai.aliyuncs.com\/naibahq\/nezha-dashboard"
-        GITHUB_RELEASE_URL="hub.fgit.cf/naiba/nezha/releases/latest/download"
-    fi
+   if [[ -z "${CN}" ]]; then
+          GITHUB_RAW_URL="raw.githubusercontent.com/naiba/nezha/master"
+          GITHUB_URL="github.com"
+          Get_Docker_URL="get.docker.com"
+          Get_Docker_Argu=" "
+          Docker_IMG="ghcr.io\/naiba\/nezha-dashboard"
+          GITHUB_RELEASE_URL="github.com/naiba/nezha/releases/latest/download"
+      else
+          GITHUB_RAW_URL="gitee.com/naibahq/nezha/raw/master"
+          GITHUB_URL="dn-dao-github-mirror.daocloud.io"
+          Get_Docker_URL="get.docker.com"
+          Get_Docker_Argu=" -s docker --mirror Aliyun"
+          Docker_IMG="registry.cn-shanghai.aliyuncs.com\/naibahq\/nezha-dashboard"
+          GITHUB_RELEASE_URL="hub.fgit.cf/naiba/nezha/releases/latest/download"
+      fi
 }
 
 confirm() {
